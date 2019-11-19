cbebd06cd1e88f45a19e7009d66e8ff69f962fa7
Charlie Yang <charlieyang@CH01207-Mbp.local>
2019-11-19 15:13:08 +0800

    fix

--- zk.go
+++ zk.go
@@ -10,7 +10,7 @@ import (
 var ZKConn *zk.Conn
 
 func InitializeZK(servers []string) {
-	zc, evt, err := zk.Connect(servers, 10*time.Second, nil)
+	zc, evt, err := zk.Connect(servers, 10*time.Second)
 	ZKConn = zc
 	if err != nil {
 		log.Fatalf("Fail to connect to ZK! %v", err)
